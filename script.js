382ac7226a01eb77b411357d5aa5296defddffcd
Karina <sdakj321@gnail.com>
2026-06-26 13:17:17 +0900

    Flexbox-вёрстка + подсветка меню

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-console.log("Привет! Скрипт подключен и работает.");
-
+// Код вывод текущей даты в подвал сайта
 const dateSpan = document.getElementById("update-date");
-const today = new Date();
-dateSpan.textContent = today.toLocaleDateString("ru-RU");
+if (dateSpan) {
+  const today = new Date();
+  dateSpan.textContent = today.toLocaleDateString("ru-RU");
+}
+
+//  Подсветка активного пункта меню по клику
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    // Сначала убираем класс active у всех пунктов меню
+    navLinks.forEach(l => l.classList.remove("active"));
+    // Добавляем класс active только тому пункту, на который кликнули
+    link.classList.add("active");
+  });
+});
