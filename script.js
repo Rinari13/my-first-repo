5f3223ca217ca7cbab1978b4fe7e2f4356f529f0
Karina <sdakj321@gnail.com>
2026-06-26 13:40:02 +0900

    Сетка Grid + адаптивное бергер-меню

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-// Код вывод текущей даты в подвал сайта
+// Код вывод текущей даты
 const dateSpan = document.getElementById("update-date");
 if (dateSpan) {
   const today = new Date();
   dateSpan.textContent = today.toLocaleDateString("ru-RU");
 }
 
-//  Подсветка активного пункта меню по клику
+// Код подсветка активного пункта меню
 const navLinks = document.querySelectorAll("nav a");
-
 navLinks.forEach(link => {
   link.addEventListener("click", () => {
-    // Сначала убираем класс active у всех пунктов меню
     navLinks.forEach(l => l.classList.remove("active"));
-    // Добавляем класс active только тому пункту, на который кликнули
     link.classList.add("active");
   });
 });
+
+// Логика открытия/закрытия бургер-меню по клику
+const burgerBtn = document.getElementById("burger-btn");
+const nav = document.querySelector("nav");
+
+if (burgerBtn && nav) {
+  burgerBtn.addEventListener("click", () => {
+    nav.classList.toggle("open");
+  });
+}
